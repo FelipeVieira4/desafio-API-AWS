2064bdfc22d783e5758d8272e9a88a7839b57ca5
Felipe Vieira4 <felipevieira58238@gmail.com>
2023-11-18 20:07:17 -0300

    feat: método procurarProximaDataPalindromo

--- source.js
+++ source.js
@@ -1,23 +1,46 @@
 
 
-const dataPalindroma = (data) => {
+const verificarDataPalindromo = (data) => {
   if (typeof data !== "string") {
-    return "ERRO";
+    return false;
   }
-
-  console.log(data);
 
   const dataArray = data.split("/");
 
-  let concatenaDiaMes = dataArray[0] + dataArray[1];//"12" + "02" = "1202"
+  let concatenacaoDiaMes = dataArray[0] + dataArray[1];//"12" + "02" = "1202"
   let anoInvertido = dataArray[2].split("").reverse().join("");//inverter string "2021" => "1202" 
 
-  if (concatenaDiaMes == anoInvertido) {
-    console.log("É um palíndromo!");
-  } else {
-    console.log("Não é um palíndromo.");
+  if (concatenacaoDiaMes == anoInvertido) {
+    return true;
   }
 
+  return false;
 };
 
-dataPalindroma("12/03/4021");
+
+const procurarProximaDataPalindromo = (data) =>{
+  const dataArray = data.split("/");
+
+  let ano = Number(dataArray[2]);
+  ano++;
+  while (true) {
+    let mes = Number(ano.toString().slice(0, 2).split("").reverse().join(""));
+    let dia = Number(ano.toString().slice(2, 4).split("").reverse().join(""));
+
+    if(dia>=13){
+      dia=1;
+      mes++;
+    }
+
+    let dataRegex = `${dia.toString().padStart(2, '0')}/${mes.toString().padStart(2, '0')}/${ano}`;
+
+    if (verificarDataPalindromo(dataRegex)) {
+      return dataRegex;
+    }
+
+    ano++;
+  }
+}
+
+verificarDataPalindromo("12/02/2021");
+console.log(procurarDataPalindromo("12/02/2021"));
